16ed2fb75c647e01c2d2a4b1bd6ec1519ba4b6cd
John Rusty Lola <johnrustylola@gmail.com>
2025-03-03 19:08:56 +0800

    add default layout from appjs

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -11,13 +11,5 @@
     const props = defineProps({
         message: String
     });
-    
-</script>
 
-<script>
-    import MainLayout from '../../Layouts/MainLayout.vue';
-
-    export default {
-        layout: MainLayout
-    }
 </script>
